76240c6fc23ee22eba6e975afc1d6d682594efd0
Karl <karl2006y@gmail.com>
2021-01-29 15:13:09 +0800

    手機板型調整完成

---
+++ js/script.js
@@ -0,0 +1,38 @@
+// const ctrOpen = (qDivName) => {
+//     console.log(this)
+//     // if (document.querySelector(`#${qDivName}`).className.indexOf("close") > -1) {
+//     //     document.querySelector(`#${qDivName}`).className = "top14Top"
+//     // } else {
+//     //     document.querySelector(`#${qDivName}`).className = "top14Top close"
+//     // }
+
+// }
+AOS.init();
+
+$(".questionQ").click(function () {
+  const dom = $(this).parent();
+  dom.siblings().addClass("closeQ");
+  if (dom.hasClass("closeQ")) {
+    dom.removeClass("closeQ");
+  } else {
+    dom.addClass("closeQ");
+  }
+});
+
+setInterval(() => {
+  let text = document.querySelector("#news span").textContent;
+  document.querySelector("#news span").textContent =
+    text.substring(1, text.length) + text.charAt(0);
+}, 600);
+
+$("#mmenubutton").click(function () {
+  $("#menulist").fadeToggle("fast");
+});
+
+$("#menulist").click(function () {
+  $("#menulist").fadeToggle("fast");
+});
+
+$(window).resize(function () {
+  console.log($(window).width());
+});
